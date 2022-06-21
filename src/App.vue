dab796d7cc62d601a857297c0315013ade12e3e3
zawadzki2778 <zawadzki2778@gmail.com>
2022-06-21 21:12:35 +0300

    created comp MyEmail

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <my-email />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyEmail from "@/component/MyEmail.vue"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { MyEmail },
+};
 </script>
 
 <style>
@@ -22,7 +18,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/MyEmail.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="root">
+    <label>
+      введите email:
+      <input type="email" :value="email" @input="addMail" />
+    </label>
+    <label>
+      введите пароль:
+      <input type="password" :value="password" @input="addPassword" />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyEmail",
+  data() {
+    return {
+      email: "m.zawadzki@icol.com",
+      password: "zawadzki2778",
+    };
+  },
+  methods: {
+    addMail(event) {
+      this.email = event.target.value;
+    },
+    addPassword(event) {
+      this.password = event.target.value;
+    }
+  },
+};
+</script>
