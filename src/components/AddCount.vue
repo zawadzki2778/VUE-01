b0d7aa77d0d46abd8748201768141f6a80562ca9
zawadzki2778 <zawadzki2778@gmail.com>
2022-07-12 15:01:04 +0300

    в AddCount добавил показ окошка при наведении курсора на title

--- src/components/AddCount.vue
+++ src/components/AddCount.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="input">
-    <h2> {{title}} </h2>
+    <h2 v-bind:title="msg"> {{title}} </h2>  <!--выведет msg при наведении курсора в отдельном окне -->
     <button type="button" @click="decrease">-</button>
     <input type="number" :value="count" :min="minValue" :max="maxValue" />
     <button type="button" @click="increase">+</button>
@@ -12,7 +12,8 @@ export default {
   name: "AddCount",
   data() {
     return {
-      title: "выберите колличество",
+      title: "выберите количество",
+      msg: "спасибо",
       minValue: 0,
       maxValue: 10,
       count: 1,
