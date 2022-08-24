ced26e1bac2c9ff3724f7e95018c868bcb57e1f8
zawadzki2778 <zawadzki2778@gmail.com>
2022-08-24 16:58:28 +0300

    подключил библиотеку bootstrap-vue

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@
 </template>
 
 <script>
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 export default {
   name: "App",
 };
--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import router from './router'
+import  BootstrapVue  from 'bootstrap-vue'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter);
+Vue.use(BootstrapVue);
 
 new Vue({
   router: router,
