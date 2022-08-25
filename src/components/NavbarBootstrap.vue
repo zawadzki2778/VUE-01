4230d5041cba449ec8341bb9ccff6835572ba415
zawadzki2778 <zawadzki2778@gmail.com>
2022-08-25 17:23:51 +0300

    изменил цвет меню на primary

--- src/components/NavbarBootstrap.vue
+++ src/components/NavbarBootstrap.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar toggleable type="dark" variant="dark">
+  <b-navbar toggleable type="dark" variant="primary">
     <b-navbar-brand href="#">УЧЕБНЫЙ ПРОЕКТ</b-navbar-brand>
 
     <b-navbar-toggle target="navbar-toggle-collapse">
