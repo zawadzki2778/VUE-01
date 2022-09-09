329eefaa502547bf119568b916e70c602071d960
zawadzki2778 <zawadzki2778@gmail.com>
2022-09-09 13:20:44 +0300

    добавил стили в app для html, body

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,12 @@ export default {
 </script>
 
 <style lang="scss">
+html {
+  height: 100%;
+}
+body {
+  height: 100%;
+}
 .app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -26,6 +32,7 @@ export default {
   margin: 0 auto;
   text-align: center;
   box-sizing: border-box;
+  height: 100%;
 }
 li {
   list-style-type: none;
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class="main">
+  <div class="img">
+    <!-- <img alt="image" /> -->
     <h1>ГЛАВНАЯ СТАНИЦА</h1>
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss" scooped>
+.img {
+  background: url("../assets/digitization-5140055_960_720.jpg") no-repeat fixed;
+  background-size: 100%;
+  height: 100%;
+}
+h1 {
+  margin-top: 20px;
+}
 </style>
