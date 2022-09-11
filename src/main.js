983194a8c2e946de8f670d3005afe859c44e04ab
zawadzki2778 <zawadzki2778@gmail.com>
2022-09-11 21:11:34 +0300

    сделал экспорты/импорты для работы со vuex

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import store from './vuex/store'
 
 Vue.config.productionTip = false
 
@@ -12,6 +13,7 @@ Vue.use(IconsPlugin);
 
 new Vue({
   router: router,
+  store: store,
   el: '#app',
   render: h => h(App)
 })
