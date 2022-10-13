7febca57a48a57b85d79e5c1623d043fc0f52c98
Maksim Zawadzki <zawadzki2778@gmail.com>
2022-10-13 11:53:29 +0300

    created comp.FormWork

---
+++ src/components/FormWork.vue
@@ -0,0 +1,42 @@
+<template>
+  <form class="sign-up">
+    <div class="form-group">
+      <label for="login">Логин:</label>
+      <input id="login" class="form-control" />
+    </div>
+
+    <div class="form-group">
+      <label for="login">Почта:</label>
+      <input id="email" type="email" class="form-control" />
+    </div>
+
+    <div class="form-group">
+      <label for="login">Пароль:</label>
+      <input id="password" type="password" class="form-control" />
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "FormWork",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.form-group {
+  margin: 30px auto;
+  text-align: left;
+  max-width: 500px;
+}
+// .form-control {
+//   width: 400px;
+// }
+
+button {
+  margin-top: 15px;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,27 +1,29 @@
-import VueRouter from 'vue-router'
-import MainPage from './components/MainPage.vue';
-import AcyncAwait from './components/AcyncAwait.vue';
-import FirstVuex from './components/FirstVuex.vue';
-import MyEmail from './components/MyEmail.vue';
-import ProductCard from './components/ProductCard.vue';
-import ProductList from './components/ProductList.vue';
-import RedactPhoto from './components/RedactPhoto.vue';
-import ToDoApp from './components/ToDoApp.vue';
-import SearchName from './components/SearchName.vue';
-import TwoForms from './components/TwoForms.vue';
+import VueRouter from "vue-router";
+import MainPage from "./components/MainPage.vue";
+import AcyncAwait from "./components/AcyncAwait.vue";
+import FirstVuex from "./components/FirstVuex.vue";
+import MyEmail from "./components/MyEmail.vue";
+import ProductCard from "./components/ProductCard.vue";
+import ProductList from "./components/ProductList.vue";
+import RedactPhoto from "./components/RedactPhoto.vue";
+import ToDoApp from "./components/ToDoApp.vue";
+import SearchName from "./components/SearchName.vue";
+import TwoForms from "./components/TwoForms.vue";
+import FormWork from "./components/FormWork.vue";
 
 export default new VueRouter({
   routes: [
-    { path: '/', component: MainPage, name: 'MainPage' },
-    { path: '/1', component: AcyncAwait, name: 'AcyncAwait' },
-    { path: '/2', component: FirstVuex, name: 'FirstVuex' },
-    { path: '/3', component: MyEmail, name: 'MyEmail' },
-    { path: '/4', component: ProductCard, name: 'ProductCard' },
-    { path: '/5', component: ProductList, name: 'ProductList' },
-    { path: '/6', component: RedactPhoto, name: 'RedactPhoto' },
-    { path: '/7', component: ToDoApp, name: 'ToDoApp' },
-    { path: '/8', component: SearchName, name: 'SearchName' },
-    { path: '/9', component: TwoForms, name: 'TwoForms' },
+    { path: "/", component: MainPage, name: "MainPage" },
+    { path: "/1", component: AcyncAwait, name: "AcyncAwait" },
+    { path: "/2", component: FirstVuex, name: "FirstVuex" },
+    { path: "/3", component: MyEmail, name: "MyEmail" },
+    { path: "/4", component: ProductCard, name: "ProductCard" },
+    { path: "/5", component: ProductList, name: "ProductList" },
+    { path: "/6", component: RedactPhoto, name: "RedactPhoto" },
+    { path: "/7", component: ToDoApp, name: "ToDoApp" },
+    { path: "/8", component: SearchName, name: "SearchName" },
+    { path: "/9", component: TwoForms, name: "TwoForms" },
+    { path: "/10", component: FormWork, name: "FormWork" },
   ],
-  mode: 'history'
-}) 
+  mode: "history",
+});
